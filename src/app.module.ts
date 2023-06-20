e259a616552fa0e0cd80f514ec149a070094634d
Oussama Seffai <oussama.seffai@legal-doctrine.com>
2023-06-20 23:06:16 +0100

    (app): add some modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProjectModule, AuthModule, UserModule],
+  imports: [
+    ProjectModule,
+    AuthModule,
+    UserModule,
+    ConfigModule.forRoot({ envFilePath: '.development.env', isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
