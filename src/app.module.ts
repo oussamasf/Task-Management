c9e9dd4a817cba835ab307273494e7ca8d1c4b28
Oussama Seffai <oussama.seffai@legal-doctrine.com>
2023-06-20 18:32:57 +0100

    feat(user): init

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectModule } from './project/project.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ProjectModule],
+  imports: [ProjectModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
