0df3c2280f2caea20b3ca15f625e246f19351d98
Oussama Seffai <oussama.seffai@legal-doctrine.com>
2023-06-20 23:49:10 +0100

    fix(auth): protect all endpoints except few

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
+import { AuthGuard } from './auth.guard';
 
+export const IS_PUBLIC_KEY = 'isPublic';
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 console.log(process.env.JWT_SECRET);
 @Module({
   imports: [
@@ -12,7 +15,13 @@ console.log(process.env.JWT_SECRET);
     UserModule,
     JwtModule.register({ global: true, secret: process.env.JWT_SECRET }),
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: 'APP_GUARD',
+      useClass: AuthGuard,
+    },
+  ],
   controllers: [AuthController],
 })
 export class AuthModule {}
