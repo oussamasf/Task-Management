a8e68198880d9ac8d46150282f27b02cb2d924f6
Oussama Seffai <oussama.seffai@legal-doctrine.com>
2023-06-21 15:52:03 +0100

    feat(auth): change error message in validation

--- src/resources/user/dto/create-user.dto.ts
+++ src/resources/user/dto/create-user.dto.ts
@@ -1,8 +1,9 @@
-import { IsString, IsEmail } from 'class-validator';
+import { IsString, IsEmail, MinLength } from 'class-validator';
 export class CreateUsertDto {
   @IsString()
   username: string;
   @IsString()
+  @MinLength(8)
   password: string;
   @IsEmail()
   email: string;
