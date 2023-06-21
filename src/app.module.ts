bf4182af868aa82182b18075c1d8e0d7f352ef46
Oussama Seffai <oussama.seffai@legal-doctrine.com>
2023-06-21 03:11:59 +0100

    refactor(main): make structure to the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProjectModule } from './project/project.module';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
+import { ProjectModule } from './resources/project/project.module';
+import { AuthModule } from './resources/auth/auth.module';
+import { UserModule } from './resources/user/user.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
