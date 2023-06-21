e93daf1c7726816fbd5498d3e9bdab260dda934d
Oussama Seffai <oussama.seffai@legal-doctrine.com>
2023-06-21 22:11:23 +0100

    feat(project):init project

--- src/resources/project/project.service.ts
+++ src/resources/project/project.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
+import { Project } from '../../schemas/project.schema';
 
 @Injectable()
 export class ProjectService {
-  create(createProjectDto: CreateProjectDto) {
-    return `This action adds a new project: ${createProjectDto.name}`;
+  constructor(@InjectModel(Project.name) private userModel: Model<Project>) {}
+
+  async create(createProjectDto: CreateProjectDto) {
+    const createdUser = await this.userModel.create(createProjectDto);
+    return `This action adds a new project: ${createdUser}`;
   }
 
   createTask(createProjectDto: CreateTaskDto) {
--- src/schemas/project.schema.ts
+++ src/schemas/project.schema.ts
@@ -1,43 +1,20 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-import * as bcrypt from 'bcrypt';
 
-export type UserDocument = HydratedDocument<User>;
+export type ProjectDocument = HydratedDocument<Project>;
 
 @Schema({
   timestamps: {
-    createdAt: 'created_at', // Use `created_at` to store the created date
-    updatedAt: 'updated_at', // and `updated_at` to store the last updated date
+    createdAt: 'createdAt',
+    updatedAt: 'updatedAt',
   },
 })
-export class User {
+export class Project {
   @Prop({ required: true })
-  username: string;
+  title: string;
 
   @Prop({ required: true })
-  password: string;
-
-  @Prop({ required: true, unique: true })
-  email: string;
-
-  @Prop({ type: [String], required: true })
-  roles: string[];
+  description: string;
 }
 
-export const UserSchema = SchemaFactory.createForClass(User);
-
-UserSchema.pre('save', async function (next) {
-  const CRYPTO_SALTORROUNDS = 10;
-  const user = this as User;
-
-  try {
-    const hashedPassword = await bcrypt.hash(
-      user.password,
-      parseInt(`${CRYPTO_SALTORROUNDS}`),
-    );
-    user.password = hashedPassword;
-    next();
-  } catch (error) {
-    return next(error);
-  }
-});
+export const ProjectSchema = SchemaFactory.createForClass(Project);
