efb6f5571f6145f7999fd8906e5313ff15d92ab1
Oussama Seffai <oussama.seffai@legal-doctrine.com>
2023-06-25 23:42:02 +0100

    fix(ci/workflow):fix env

--- src/utils/config/app/index.ts
+++ src/utils/config/app/index.ts
@@ -6,8 +6,8 @@ const { NODE_ENV, MONGO_URI } = process.env;
 export const config =
   NODE_ENV === 'test'
     ? {
-        envFilePath: '.development.env',
-        dbUri: 'mongodb://localhost:27014/nestauth_test',
+        envFilePath: '.test.env',
+        dbUri: MONGO_URI,
       }
     : {
         envFilePath: '.development.env',
